85345c4cac0d14364fa0063f2c2142aa42ad1084
sklobo <sujay.lobo@student.unimelb.edu.au>
2026-03-16 17:17:03 +1100

    fi

--- scripts/creature_states/wolf_idle.gd
+++ scripts/creature_states/wolf_idle.gd
@@ -6,6 +6,9 @@ func update(delta):
 	var nearest_entity = master.get_nearest_entity(detect_range)
 	var nearest_alpha = master.get_nearby(detect_range, "Wolf Alpha")
 	var nearest_deadbody = master.get_nearest_corpse(detect_range)
+	var potential_hostile = master.get_nearest_hostile(detect_range)
+	if potential_hostile:
+		transitioned.emit(self,StateType.CHASE, {"target": potential_hostile})
 	if nearest_entity:
 		if master.is_hostile_to(nearest_entity.type):
 			master.form_mark(load("res://assets/UI/white_exclamation.png"))
